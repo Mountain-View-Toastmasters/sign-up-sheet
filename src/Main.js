5296bbe5eb56a74a7751aa610a2c84d4a4bd00f3
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-04-03 13:50:50 -0700

    Add alert for confirming

--- src/Main.js
+++ src/Main.js
@@ -12,9 +12,30 @@ function copyAndGenerateMinutes() {
   generateMinutes();
 }
 
+// https://developers.google.com/apps-script/guides/dialogs
+function showAlert() {
+  var ui = SpreadsheetApp.getUi(); // Same variations.
+
+  var result = ui.alert(
+    "Please confirm",
+    "Are you sure you want to continue?",
+    ui.ButtonSet.YES_NO
+  );
+
+  // Process the user's response.
+  if (result == ui.Button.YES) {
+    // User clicked "Yes".
+    ui.alert("Confirmation received.");
+  } else {
+    // User clicked "No" or X in the title bar.
+    ui.alert("Permission denied.");
+  }
+}
+
 function clearAndAdvanceSignUp() {
-  clearToastmasterDetails();
-  advanceSignUpSheet();
+  showAlert();
+  // clearToastmasterDetails();
+  // advanceSignUpSheet();
 }
 
 function onOpen() {
